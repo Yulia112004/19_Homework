62e877d0a63aa30caef995315433e174c0b6242c
Yulia Byckova <byckovaulia669@gmail.com>
2023-12-18 22:39:24 +0300

    Done Homework 19_3

--- Homework_19/catalog/models.py
+++ Homework_19/catalog/models.py
@@ -1,19 +1,18 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, verbose_name='наименование')
+    category_name = models.CharField(max_length=100, verbose_name='наименование')
     disc = models.CharField(max_length=200, verbose_name='описание')
-    created_at = models.IntegerField()
 
     def __str__(self):
-        return  f'{self.name} ({self.disc})'
+        return  f'{self.category_name}'
 
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
 
 class Product(models.Model):
-    name = models.CharField(max_length=100, verbose_name='наименование')
+    product_name = models.CharField(max_length=100, verbose_name='наименование')
     product_disc = models.CharField(max_length=200, verbose_name='описание')
     image = models.ImageField(upload_to='products/', verbose_name='изображение')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
@@ -22,11 +21,19 @@ class Product(models.Model):
     data_changed = models.DateTimeField(verbose_name='дата последнего изменения')
 
     def __str__(self):
-        return f'{self.name}'
+        return f'{self.product_name} - {self.price} ({self.category})'
 
     class Meta:
         verbose_name = 'товар'
         verbose_name_plural = 'товары'
 
+
+class Contact(models.Model):
+    country = models.CharField(max_length=100)
+    inn = models.CharField(max_length=12)
+    address = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return f"{self.country} ({self.address})"
 
 
